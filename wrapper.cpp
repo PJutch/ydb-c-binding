51f40486e7b06947792dd4281570c95d91cbf6c1
PJutch <paveljutch@yandex.ru>
2026-04-08 20:59:57 +0300

    Write a very basic implementation

---
+++ wrapper.cpp
@@ -0,0 +1,66 @@
+#include "wrapper.h"
+
+#include <ydb-cpp-sdk/client/driver/driver.h>
+#include <ydb-cpp-sdk/client/query/query.h>
+#include <ydb-cpp-sdk/client/query/client.h>
+
+extern "C" {
+
+struct TDriverConfig {
+    NYdb::TDriverConfig config{};
+};
+
+TDriverConfig* CreateDriverConfig() {
+    return new TDriverConfig{};
+}
+
+TDriverConfig* CreateDriverConfigConnectionString(char* connectionString) {
+    return new TDriverConfig{{connectionString}};
+}
+
+void DestroyDriverConfig(TDriverConfig* config, char* endpoint) {
+    delete config;
+}
+
+void DriverConfigSetEndpoint(TDriverConfig* config, char* endpoint) {
+    config->config.SetEndpoint(endpoint);
+}
+
+void DriverConfigSetDatabase(TDriverConfig* config, char* database) {
+    config->config.SetDatabase(database);
+}
+
+void DriverConfigUseSecureConnection(TDriverConfig* config, char* caCert) {
+    config->config.UseSecureConnection(caCert);
+}
+
+struct TDriver {
+    NYdb::TDriver driver;
+};
+
+TDriver* CreateDriver(TDriverConfig* config) {
+    return new TDriver{{config->config}};
+}
+
+void DestroyDriver(TDriver* driver) {
+    delete driver;
+}
+
+void Stop(TDriver* driver, bool wait) {
+    driver->driver.Stop(wait);
+    delete driver;
+}
+
+struct TQueryClient {
+    NYdb::NQuery::TQueryClient client;
+};
+
+TQueryClient* CreateQueryClient(TDriver* driver) {
+    return new TQueryClient{{driver->driver}};
+}
+
+void DestroyQueryClient(TQueryClient* client) {
+    delete client;
+}
+
+}
---
+++ wrapper.h
@@ -0,0 +1,33 @@
+#ifndef WRAPPER_H_
+#define WRAPPER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct TDriverConfig TDriverConfig;
+
+TDriverConfig* CreateDriverConfig();
+TDriverConfig* CreateDriverConfigConnectionString(char* connectionString);
+
+void DestroyDriverConfig(TDriverConfig* config, char* endpoint);
+
+void DriverConfigSetEndpoint(TDriverConfig* config, char* endpoint);
+void DriverConfigSetDatabase(TDriverConfig* config, char* database);
+void DriverConfigUseSecureConnection(TDriverConfig* config, char* caCert);
+
+typedef struct TDriver TDriver;
+
+TDriver* CreateDriver(TDriverConfig* config);
+void StopDriver(TDriver* driver, bool wait);
+
+typedef struct TQueryClient TQueryClient;
+
+TQueryClient* CreateQueryClient(TDriver* driver);
+void DestoryQueryClient(TQueryClient* client);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
