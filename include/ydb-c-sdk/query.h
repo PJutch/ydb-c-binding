db4f8cec9da8f3e6599cfa121c0e8b7d17607624
PJutch <paveljutch@yandex.ru>
2026-04-09 23:36:18 +0300

    Implement sync queries without results

--- include/ydb-c-sdk/query.h
+++ include/ydb-c-sdk/query.h
@@ -3,6 +3,8 @@
 
 #include "driver.h"
 
+#include <stdbool.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -11,6 +13,31 @@ typedef struct TQueryClient TQueryClient;
 
 TQueryClient* CreateQueryClient(TDriver* driver);
 void DestroyQueryClient(TQueryClient* client);
+
+typedef void* TStatus;
+
+void DestroyStatus(TStatus status);
+
+bool IsSuccess(TStatus status);
+bool IsTransportError(TStatus status);
+
+char* GetErrorMessage(TStatus status);
+void DestroyErrorMessage(char* message);
+
+typedef void* TQueryResult;
+
+void DestroyResult(TQueryResult result);
+
+// Doesn't copy, don't destroy both
+TStatus AsStatus(TQueryResult result);
+
+typedef void* TSession;
+
+TQueryResult ExecuteQuerySync(TSession session, char* query);
+
+typedef TStatus (*SyncRetryable) (TSession session, void* data);
+
+TStatus RetryQuerySync(TQueryClient* client, SyncRetryable query, void* data);
 
 #ifdef __cplusplus
 }
--- src/query.cpp
+++ src/query.cpp
@@ -1,7 +1,10 @@
 #include "driver.hpp"
+#include "ydb-c-sdk/query.h"
 
 #include <ydb-cpp-sdk/client/query/query.h>
 #include <ydb-cpp-sdk/client/query/client.h>
+#include <__ydb_sdk_special_headers/util/stream/str.h>
+#include <__ydb_sdk_special_headers/util/generic/string.h>
 
 extern "C" {
 
@@ -15,6 +18,59 @@ TQueryClient* CreateQueryClient(TDriver* driver) {
 
 void DestroyQueryClient(TQueryClient* client) {
     delete client;
+}
+
+void DestroyStatus(TStatus status) {
+    delete static_cast<NYdb::TStatus*>(status);
+}
+
+bool IsSuccess(TStatus status) {
+    return static_cast<NYdb::TStatus*>(status)->IsSuccess();
+}
+
+bool IsTransportError(TStatus status) {
+    return static_cast<NYdb::TStatus*>(status)->IsTransportError();
+}
+
+char* GetErrorMessage(TStatus status) {
+    TString string;
+    TStringOutput stream{string};
+
+    static_cast<NYdb::TStatus*>(status)->Out(stream);
+
+    return strdup(string.data());
+}
+
+void DestroyErrorMessage(char* message) {
+    free(message);
+}
+
+void FreeResult(TQueryResult* result) {
+    free(result);
+}
+
+void* AsStatus(TQueryResult result) {
+    auto* as_result = static_cast<NYdb::NQuery::TExecuteQueryResult*>(result);
+    auto* as_status = static_cast<NYdb::TStatus*>(as_result);
+    return static_cast<void*>(as_status);
+}
+
+TQueryResult ExecuteQuerySync(TSession session_, char* query) {
+    auto* session = static_cast<NYdb::NQuery::TSession*>(session_);
+    auto result = session->ExecuteQuery(query, NYdb::NQuery::TTxControl::NoTx()).GetValueSync();
+    return static_cast<void*>(new NYdb::NQuery::TExecuteQueryResult{std::move(result)});
+}
+
+TStatus RetryQuerySync(TQueryClient* client, SyncRetryable query, void* data) {
+    NYdb::TStatus status = client->client.RetryQuerySync([query, data](NYdb::NQuery::TSession session) {
+        NYdb::TStatus* status = static_cast<NYdb::TStatus*>(query(static_cast<TSession>(&session), data));
+
+        NYdb::TStatus status_value = std::move(*status);
+        delete status;
+        return status_value;
+    });
+
+    return static_cast<void*>(new NYdb::TStatus(std::move(status)));
 }
 
 }
